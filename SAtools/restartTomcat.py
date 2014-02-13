926a60d8057b188e0c275ab43dee6317b901a46b
lcerezo <luis.cerezo@gmail.com>
2014-02-13 14:54:28 -0700

    add tool for restarting tomcat apps remotely

---
+++ SAtools/restartTomcat.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import paramiko, os, sys, glob, fnmatch
+
+optoutfilelist = glob.glob('/tmp/*.csv')
+targetdir = ('/dev/shm/')
+paramiko.util.log_to_file('./p.logs')
+srcdir = ('/tmp/')
+hostlist = [ fsm11-dev, fsm12-dev]
+ssh = paramiko.SSHClient()
+ssh.load_system_host_keys()
+ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+def pushoptouts(host):
+	optoutfiles = getfilestopush(srcdir)
+	ssh.connect(host)
+	sftp = ssh.open_sftp()
+	for file in optoutfiles:
+		srcfullpath = ( srcdir + file )
+		destfullpath = (targetdir + file )
+	#	#sftp.put(file, str.join(targetdir,file))
+		sftp.put(srcfullpath, destfullpath)
+		#print srcfullpath,  destfullpath
+	sftp.close()
+	
+def sshcontrol(host, user2, runcmd):
+	client = paramiko.SSHClient()
+	client.load_system_host_keys()
+	client.connect(host, username=user2)
+	stdin, stdout, stderr = client.exec_command(runcmd)
+	#print "stderr: ", stderr.readlines()
+	print stdout.readlines()
+
+	
