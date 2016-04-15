68853108d411e8f0cef06a51e26d4afef70a1944
luis cerezo <luis.cerezo@gmail.com>
2016-04-15 12:18:22 -0600

    learning how to test shit.

--- SAtools/victoropstool/posttoVO.py
+++ SAtools/victoropstool/posttoVO.py
@@ -22,20 +22,28 @@ args = vars(parser.parse_args())
 opts = parser.parse_args()
 
 
-def gen_url(myapikey, myroutekey):
-    baseurl = 'https://alert.victorops.com/integrations/generic/20131114/alert'
-    myurl = '%s/%s/%s' % (baseurl, myapikey, myroutekey)
-    return myurl
-
-def post_message(myurl, payload):
-    response = requests.post(myurl, data=payload)
-    return response.text, response.status_code
-
+class postToVO(object):
+    
+    def gen_url(self, myapikey, myroutekey):
+        baseurl = 'https://alert.victorops.com/integrations/generic/20131114/alert'
+        myurl = '%s/%s/%s' % (baseurl, myapikey, myroutekey)
+        return myurl
+    
+    def post_message(self, myurl, payload):
+        try:
+            response = requests.post(myurl, data=payload)
+        except PostError, err:
+            print 'Error posting to Victoops %s \n\nresponse was %s\n HTTP code was %s\n\n' % (err, response.text, response.status_code)
+        else:
+             return response.text, response.status_code
+    
 if __name__ == "__main__":
         try:
             myjson = json.dumps(args)
-            thisurl = gen_url(opts.apikey, opts.routekey)
-            rt, rsc = post_message(thisurl, myjson)
+            print myjson
+            myIncident = postToVO()
+            thisurl = myIncident.gen_url(opts.apikey, opts.routekey)
+            rt, rsc = myIncident.post_message(thisurl, myjson)
             if rsc != 200:
                 print 'ERROR : %s %s ' % (rt, rsc)
             else:
---
+++ SAtools/victoropstool/tests/test_posttoVO.py
@@ -0,0 +1,5 @@
+import os.path
+import unittest
+from urlparse import urlparse
+
+from 
