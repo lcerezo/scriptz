0f79e16573550353d8b42780a7a8ea7b851a6cb4
luis cerezo <luis.cerezo@gmail.com>
2016-04-21 10:07:45 -0600

    Adding the shell script I am trying to use on Redhat 5 types that have a shitty python install. These are called by swatch. THe syslog messages don't load into the json payload correctly due to all the " and ; and : and.. and..

---
+++ SAtools/victoropstool/posttoVO.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+unset LD_LIBRARY_PATH
+mycurl=`which curl`
+logmessage=$4
+level=$2
+hostnameSource=$1
+VOAPIKEY=SOMEAPIKEY
+ROUTING_KEY=$3
+mylogmessage=`sed s/\"//g $logmessage`
+$mycurl --data-binary "{\"message_type\":\"$level\",\"monitoring_tool\":\"Swatch\",\"state_message\":\"$mylogmessage\",\"entity_id\":\"$hostnameSource\"}" "https://alert.victorops.com/integrations/generic/20131114/alert/$VOAPIKEY/$ROUTING_KEY" >> /tmp/posttovo.log
