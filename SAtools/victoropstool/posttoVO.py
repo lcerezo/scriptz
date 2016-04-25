689945964cb72c2a9364b3936682d7243d440b13
luis cerezo <luis.cerezo@gmail.com>
2016-04-25 13:22:20 -0600

    Added arguements to create entitiy id from swatch values since swatch was not concatinating multiple values as expected. continue to learn testing. NO idea what I'm doing here ...

--- SAtools/victoropstool/posttoVO.py
+++ SAtools/victoropstool/posttoVO.py
@@ -1,4 +1,4 @@
-#!/bin/env python27
+#!/bin/env python2.7
 
 
 import requests
@@ -12,12 +12,16 @@ parser.add_argument('--routekey', action='store', dest='routekey', required=True
                             help="the Victorops routing key")
 parser.add_argument('--mtype', action='store', dest='message_type', required=True,
                             help="maps to message_type in the dictionary. Correct values include INFO, WARNING, ACKNOWLEDGEMENT, CRITICAL, RECOVERY")
-parser.add_argument('--entityId', action='store', dest='entity_id', required=True,
+parser.add_argument('--entityId', action='store', dest='entity_id', required=False,
                             help="maps to entity_id in the dictionary this is the key to move through alert lifecycle.")
 parser.add_argument('-m', action='store', dest='state_message', required=True,
                             help="actual log error or status")
 parser.add_argument('--tool', action='store', dest='monitoring_tool', required=False,
                             help="tool generating the alert")
+parser.add_argument('--hostname', action='store', dest='hostname', required=False,
+                            help="hostname of log")
+parser.add_argument('--swatchId', action='store', dest='swatchId', required=False,
+                            help="swatch code")
 args = vars(parser.parse_args())
 opts = parser.parse_args()
 
@@ -39,8 +43,10 @@ class postToVO(object):
     
 if __name__ == "__main__":
         try:
-            myjson = json.dumps(args)
-            print myjson
+            args['entity_id'] = "%s::%s" % (opts.hostname, opts.swatchId)
+            myjson = json.dumps(args, sort_keys=True, indent=4)
+#            print myjson
+# uncomment the above line to print json payload to console to validate it is doing what you want.
             myIncident = postToVO()
             thisurl = myIncident.gen_url(opts.apikey, opts.routekey)
             rt, rsc = myIncident.post_message(thisurl, myjson)
