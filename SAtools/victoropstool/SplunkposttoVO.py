11437fabf5f48655575ce6869b3e1198f48d2ca7
luis cerezo <luis.cerezo@gmail.com>
2016-05-11 22:19:51 -0600

    TIL: I suck at this. thank you pylint. :-X

--- SAtools/victoropstool/SplunkposttoVO.py
+++ SAtools/victoropstool/SplunkposttoVO.py
@@ -1,27 +1,31 @@
 #!/bin/env python2.6
+"""
+setting the named args to more friendly names
+http://docs.splunk.com/Documentation/Splunk/6.0/alert/ConfiguringScriptedAlerts
+Inspired by http://victorops.force.com/knowledgebase/articles/Integration/Splunk-Integration
+"""
 
 
-import requests
 import json
 import syslog
 import sys
+import requests
 # this next section could be better suited to a external file.
 VOAPIKEY = 'YOURAPIKEY'
 ROUTING_KEY = 'splunk'
-level = 'critical'
-# setting the named args to more friendly names
-# http://docs.splunk.com/Documentation/Splunk/6.0/alert/ConfiguringScriptedAlerts
-# inspired by http://victorops.force.com/knowledgebase/articles/Integration/Splunk-Integration
+LEVEL = 'critical'
 
-
-class postToVO(object):
+class PostSplunkAlertToVictorOps(object):
+    """ this is to take the stuff from Splunk triggered alerts and victor ops them"""
 
     def gen_url(self, myapikey, myroutekey):
+        """ Generates the url with api key and route key """
         baseurl = 'https://alert.victorops.com/integrations/generic/20131114/alert'
         myurl = '%s/%s/%s' % (baseurl, myapikey, myroutekey)
         return myurl
 
     def post_message(self, myurl, payload):
+        """ Posts the message with json payload to victorops url endpoint"""
         try:
             response = requests.post(myurl, data=payload)
             return response.text, response.status_code
@@ -29,39 +33,53 @@ class postToVO(object):
             error = "connection error, error was\n{0}".format(err)
             raise SystemExit(error)
         except Exception, err:
-            error = 'Error posting to VictorOps %s \n\nresponse was %s\n HTTP code was %s\n\n' % (err, response.text, response.status_code)
-            raise SystemExit(error)
+            post_error = '''Error posting to VictorOps %s \n\nresponse was %s\n
+            HTTP code was %s\n\n''' % (
+                err,
+                response.text,
+                response.status_code)
+            raise SystemExit(post_error)
 
     def build_json_object(self, **kwargs):
+        """ Builds a json object from key value pairs of unkown length """
         if kwargs is not None:
-            myjson = json.dumps(kwargs)
-        return myjson
+            my_json = json.dumps(kwargs)
+        return my_json
 
 if __name__ == "__main__":
     try:
-        myIncident = postToVO()
-        thisurl = myIncident.gen_url(VOAPIKEY, ROUTING_KEY)
-        myPayload = myIncident.build_json_object(
-                splunkscriptname=sys.argv[0],
-                splunkNumberEventsReturned=sys.argv[1],
-                splunkSearchTerms=sys.argv[2],
-                splunkFullyQualifiedQueryString=sys.argv[3],
-                splunkReportName=sys.argv[4],
-                splunkTriggerReason=sys.argv[5],
-                splunkUrlToReport=sys.argv[6],
-                entity_id="{0}::{1}".format(sys.argv[4], sys.argv[5]),
-                state_message="Splunk URL to Report: {} ".format(sys.argv[5]),
-                message_type=level
-                )
-        httpResponseText, httpResponseCode = myIncident.post_message(thisurl, myjson)
-        if httpResponseCode == 200:
-            jsonResponse = json.loads(httpResponseText)
-            mylog = "IGNORE PAGED posted to VictorOps for entityId {0} Result was {1} HTTP_RESPONSE: {2} ".format(jsonResponse['entity_id'], jsonResponse['result'], httpResponseCode)
-            syslog.syslog(syslog.LOG_INFO, mylog)
+        MY_INCIDENT = PostSplunkAlertToVictorOps()
+        THIS_URL = MY_INCIDENT.gen_url(VOAPIKEY, ROUTING_KEY)
+        MY_PAYLOAD = MY_INCIDENT.build_json_object(
+            splunkscriptname=sys.argv[0],
+            splunkNumberEventsReturned=sys.argv[1],
+            splunkSearchTerms=sys.argv[2],
+            splunkFullyQualifiedQueryString=sys.argv[3],
+            splunkReportName=sys.argv[4],
+            splunkTriggerReason=sys.argv[5],
+            splunkUrlToReport=sys.argv[6],
+            entity_id="{0}::{1}".format(sys.argv[4], sys.argv[5]),
+            state_message="Splunk URL to Report: {} ".format(sys.argv[5]),
+            message_type=LEVEL
+            )
+        HTTP_RESPONSE_TEXT, HTTP_RESPONSE_CODE = MY_INCIDENT.post_message(THIS_URL, MY_PAYLOAD)
+        if HTTP_RESPONSE_CODE == 200:
+            JSON_RESPONSE = json.loads(HTTP_RESPONSE_TEXT)
+            MY_LOG = '''IGNORE PAGED posted to VictorOps for entityId {0}
+            Result was {1} HTTP_RESPONSE: {2} '''.format(
+                JSON_RESPONSE['entity_id'],
+                JSON_RESPONSE['result'],
+                HTTP_RESPONSE_CODE)
+            syslog.syslog(syslog.LOG_INFO, MY_LOG)
         else:
-            myNotJsonPayload = json.dumps(myPayload)
-            mylog = "IGNORE Failed to post to VictorOps for  entityId {0} HTTP_RESPONSE_CODE: {1} LOG: {2} HTTP_RESPONSE_TEXT: {3}".format(myNotJsonPayload['entity_id'], httpResponseCode, myNotJsonPayload['state_message'], httpResponseText)
-            syslog.syslog(syslog.LOG_WARNING, mylog)
+            MY_NOT_JSON_PAYLOAD = json.dumps(MY_PAYLOAD)
+            MY_LOG = '''IGNORE Failed to post to VictorOps for  entityId {0}
+            HTTP_RESPONSE_CODE: {1} LOG: {2} HTTP_RESPONSE_TEXT: {3}'''.format(
+                MY_NOT_JSON_PAYLOAD['entity_id'],
+                HTTP_RESPONSE_CODE,
+                MY_NOT_JSON_PAYLOAD['state_message'],
+                HTTP_RESPONSE_TEXT)
+            syslog.syslog(syslog.LOG_WARNING, MY_LOG)
     except Exception, err:
-        error = 'Failed to make a magical unicorn rainbows error was %s ' % (err)
-        raise SystemExit(error)
+        MAIN_ERROR = 'Failed to make a magical unicorn rainbows error was %s ' % (err)
+        raise SystemExit(MAIN_ERROR)
